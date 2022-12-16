3cf231cdb00d19e1b232f0e6272711191a953098
zjafarian <zjafarian88@gmail.com>
2022-12-16 21:49:19 +0330

    Initial commit

---
+++ src/main/java/com/wallet/DigiPay/DigiPayApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.DigiPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallet/DigiPay/DigiPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.DigiPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
