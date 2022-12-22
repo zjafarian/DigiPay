2ce3f5b4af8800fdf08eba547c72f0936e7cb054
zjafarian <zjafarian88@gmail.com>
2022-12-22 12:59:09 +0330

    tests are ok

--- src/test/java/com/wallet/DigiPay/digiTest/digiPayTest.java
+++ src/test/java/com/wallet/DigiPay/digiTest/digiPayTest.java
@@ -1,6 +1,8 @@
 package com.wallet.DigiPay.digiTest;
 
 import com.wallet.DigiPay.DigiPayApplication;
+import com.wallet.DigiPay.entities.Role;
+import com.wallet.DigiPay.entities.RoleType;
 import com.wallet.DigiPay.entities.User;
 import com.wallet.DigiPay.exceptions.NotFoundException;
 import com.wallet.DigiPay.repositories.UserRepository;
@@ -32,32 +34,34 @@ public class digiPayTest {
 
     @DisplayName("test method save user")
     @Test
-    public void should_When_Save_Service_Return_Entity() throws Exception {
-     /*   User user = new User();
+    public void should_When_Save_User_In_User_Service_And_Return_Entity() throws Exception {
+        User user = new User();
         user.setPhoneNumber("09127615507");
         user.setNationalCode("0080554113");
         user.setPassword("zj@50454@ZJ");
+        user.setRole(new Role(null, RoleType.User));
 
        User userSave = userService.save(user);
-        assertThat(userSave).isNotNull();*/
+        assertThat(userSave).isNotNull();
     }
 
     @DisplayName("test method save when null")
     @Test
-    public void should_When_Save_Service_Return_Entity_With_Null() {
-      /*  Assertions.assertThrows(InvalidDataAccessApiUsageException.class,
-                () -> userService.save(null));*/
+    public void should_When_Pars_Null_For_Save_Entity_In_User_Service_And_Return_Entity_With_Null() {
+        Assertions.assertThrows(InvalidDataAccessApiUsageException.class,
+                () -> userService.save(null));
 
     }
 
     @Test
     public void update_Entity_Service() throws Exception {
-     /*   User user = new User();
+        User user = new User();
 
         user.setName("zeinab");
         user.setPhoneNumber("09332844658");
         user.setNationalCode("0023518723");
         user.setPassword("z@505050@J");
+        user.setRole(new Role(null,RoleType.User));
 
         User userSave = userService.save(user);
         System.out.println(userSave.getNationalCode());
@@ -67,13 +71,13 @@ public class digiPayTest {
         User userSave2 = userService.save(userSave);
         System.out.println(userSave2.getNationalCode());
         System.out.println(userSave2.getLastName());
-        Assertions.assertTrue(userSave2.getId() == userSave.getId());*/
+        Assertions.assertTrue(userSave2.getId() == userSave.getId());
     }
 
     @Test
     public void should_Return_Throw_Exception_Not_Found_Entity() {
-      /*  Assertions.assertThrows(NotFoundException.class,
-                () -> userService.findById(200L));*/
+        Assertions.assertThrows(NotFoundException.class,
+                () -> userService.findById(200L));
     }
 
 
@@ -81,17 +85,18 @@ public class digiPayTest {
 
     @Test
     public void should_When_Username_And_Password_Return_Entity() throws Exception {
-       /* User user = new User();
+        User user = new User();
 
         user.setName("zeinab");
         user.setPhoneNumber("09332844658");
         user.setNationalCode("0023518723");
         user.setPassword("z@505050@J");
+        user.setRole(new Role(null,RoleType.User));
         User userSave = userService.save(user);
         Assertions.assertAll(
                 () -> Assertions.assertEquals(userSave.getPassword(), "09332844658"),
                 () -> Assertions.assertEquals(userSave.getPassword(), "z@505050@J")
-        );*/
+        );
     }
 
 }
